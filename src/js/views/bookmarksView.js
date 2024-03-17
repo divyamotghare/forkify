5111543e0f09da332fe8079c0d8abd8432b7637d
Divya Motghare <divyamotghare9@gmail.com>
2024-03-17 21:42:34 +0530

    refractored

--- src/js/views/bookmarksView.js
+++ src/js/views/bookmarksView.js
@@ -1,23 +1,21 @@
-import View from "./View.js";
-import previewView from "./previewView.js";
-import icons from "url:../../img/icons.svg";
+import View from './View.js';
+import previewView from './previewView.js';
+import icons from 'url:../../img/icons.svg'; // Parcel 2
 
 class BookmarksView extends View {
-  _parentElement = document.querySelector(".bookmarks__list");
-  _errorMessage = "No bookmarks yet! Find a nice recipe and bookmark it.";
-  _message = "";
+  _parentElement = document.querySelector('.bookmarks__list');
+  _errorMessage = 'No bookmarks yet. Find a nice recipe and bookmark it ;)';
+  _message = '';
 
   addHandlerRender(handler) {
-    window.addEventListener("load", handler);
+    window.addEventListener('load', handler);
   }
+
   _generateMarkup() {
-    console.log(this._data);
     return this._data
-      .map((bookmark) => previewView.render(bookmark, false))
-      .join("");
+      .map(bookmark => previewView.render(bookmark, false))
+      .join('');
   }
 }
 
 export default new BookmarksView();
-
-// console.log("hello");
